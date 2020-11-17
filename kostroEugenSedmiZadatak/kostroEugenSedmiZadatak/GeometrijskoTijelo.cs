563a551590ef61ab433504c170beb5dedd1e594a
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2020-11-17 10:54:21 +0100

    Add files via upload

---
+++ kostroEugenSedmiZadatak/kostroEugenSedmiZadatak/GeometrijskoTijelo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenSedmiZadatak
+{
+    class GeometrijskoTijelo
+    {
+        double Volumen;
+        double Oplosje;
+        static int BrojacTijela;
+        public GeometrijskoTijelo()
+        {
+            BrojacTijela++;
+        }
+        public override string ToString()
+        {
+            string vrijednost = "vrijednost je " + BrojacTijela;
+            return vrijednost;
+        }
+            
+    }
+}
---
+++ kostroEugenSedmiZadatak/kostroEugenSedmiZadatak/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenSedmiZadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kugla x = new Kugla();
+            Console.WriteLine(x);
+            Kocka y = new Kocka();
+            Console.WriteLine(y);
+            Kvadar z = new Kvadar();
+            Console.WriteLine(z);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
